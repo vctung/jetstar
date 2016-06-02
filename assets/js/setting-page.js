85f4eb86b757e5c04037c72bc61ad16dbe650c25
vctung <vctung@outlook.com>
2016-06-02 17:23:45 +0700

    finish credit card
    finish credit card

--- assets/js/setting-page.js
+++ assets/js/setting-page.js
@@ -7,3 +7,23 @@ $('.btn-canel-fields').click(function(event) {
 		___parent = $(this).parent().parent().parent();
 		___parent.removeClass('edit-mode');
 });
+
+$('body').on('click', '.btn-show-box', function(event) {
+	event.preventDefault();
+	
+	var _target = $(this).data('target');
+	$(_target).slideDown('slow', function() {
+		
+	});
+	
+});
+
+$('body').on('click', '.btn-close-box', function(event) {
+	event.preventDefault();
+	
+	var _target = $(this).data('target');
+	$(_target).slideUp('slow', function() {
+		
+	});
+	
+});
